91713cc0d83a2d8f06263fee85c3ed731130960b
Salah <salah@alefeducation.com>
2018-09-10 14:42:24 +0400

    nwe changes

--- packages/assessments/index.js
+++ packages/assessments/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Assessments = props => (
     <div>
-        <h1>Assessments ! ! !</h1>
+        <h1>Brand New Assessments</h1>
         <p>{props.name}</p>
     </div>
 );
